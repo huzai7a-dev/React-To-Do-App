4f1c424d6c2d62747c2d56b8ad6790114f6aba44
huzaifa <shaikhhuzaifa072@gmail.com>
2020-10-31 12:56:43 +0500

    Updates

---
+++ precache-manifest.fe3ca6345603908db0b86cc1d0c1fc6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d75b9cc56d59ae559efc553aad75bf1a",
+    "url": "/React-To-Do-App/index.html"
+  },
+  {
+    "revision": "e8c30e77c911f50cc9ed",
+    "url": "/React-To-Do-App/static/css/main.b85e104b.chunk.css"
+  },
+  {
+    "revision": "308e84e2e53f63730470",
+    "url": "/React-To-Do-App/static/js/2.6700c71d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React-To-Do-App/static/js/2.6700c71d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8c30e77c911f50cc9ed",
+    "url": "/React-To-Do-App/static/js/main.668de9e1.chunk.js"
+  },
+  {
+    "revision": "74c7bf7a81dce3f44a25",
+    "url": "/React-To-Do-App/static/js/runtime-main.8ffd1b4c.js"
+  }
+]);
